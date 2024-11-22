94cc6419c5a49eee087f1b82174ee0ca5ce1db0f
Uiratan Cavalcante <uiratan@gmail.com>
2024-11-22 17:56:49 -0300

    refactor(domain): remove stack trace from domain exceptions
    Reason: Reduce log noise and maintain simplicity for controlled exceptions in the domain.

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/validation/handler/ThrowsValidationHandler.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/validation/handler/ThrowsValidationHandler.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ThrowsValidationHandler implements ValidationHandler {
     @Override
     public ValidationHandler append(final Error anError) {
-        throw DomainException.with(List.of(anError));
+        throw DomainException.with(anError);
     }
 
     @Override
@@ -18,11 +18,11 @@ public class ThrowsValidationHandler implements ValidationHandler {
     }
 
     @Override
-    public ValidationHandler validate(Validation aValidation) {
+    public ValidationHandler validate(final Validation aValidation) {
         try {
             aValidation.validate();
         } catch (final Exception ex) {
-            throw DomainException.with(List.of(new Error(ex.getMessage())));
+            throw DomainException.with(new Error(ex.getMessage()));
         }
 
         return this;
