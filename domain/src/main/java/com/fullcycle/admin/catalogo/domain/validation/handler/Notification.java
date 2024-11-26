c7889ffada69bbfee66f03eaee92506e2f07a7eb
Uiratan Cavalcante <uiratan@gmai..com>
2024-11-26 14:52:19 -0300

    feat(domain): implementing notification pattern as a validation handler

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/validation/handler/Notification.java
@@ -0,0 +1,54 @@
+package com.fullcycle.admin.catalogo.domain.validation.handler;
+
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.Error;
+import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification implements ValidationHandler {
+
+    private final List<Error> errors;
+
+    private Notification(final List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public static Notification create() {
+        return new Notification(new ArrayList<>());
+    }
+
+    public static Notification create(final Error anError) {
+        return new Notification(new ArrayList<>()).append(anError);
+    }
+
+    @Override
+    public Notification append(final Error anError) {
+        this.errors.add(anError);
+        return this;
+    }
+
+    @Override
+    public Notification append(final ValidationHandler aHandler) {
+        this.errors.addAll(aHandler.getErrors());
+        return null;
+    }
+
+    @Override
+    public Notification validate(final Validation aValidation) {
+        try {
+            aValidation.validate();
+        } catch (DomainException ex) {
+            this.errors.addAll(ex.getErrors());
+        } catch (Throwable t) {
+            this.errors.add(new Error(t.getMessage()));
+        }
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return this.errors;
+    }
+}
