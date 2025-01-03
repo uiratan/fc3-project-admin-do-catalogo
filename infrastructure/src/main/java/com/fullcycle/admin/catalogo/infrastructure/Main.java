23b3cbdbbb30f47d8c44bcbfbffd08244b710050
Uiratan Cavalcante <uiratan@gmai..com>
2025-01-03 20:40:01 -0300

    docs(infrastructure/category/api): including springdocs openapi library

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -22,7 +22,7 @@ import java.util.List;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 
