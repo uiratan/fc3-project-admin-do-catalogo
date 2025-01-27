17979a42e8c5acb1d0d4fcbc357b4edf31bfb5e5
Uiratan Cavalcante <uiratan@gmai..com>
2025-01-27 11:27:17 -0300

    feat(application/genre): implementing the genre listing use case

--- application/src/test/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseTest.java
@@ -69,7 +69,7 @@ public class ListGenreUseCaseTest extends UseCaseTest {
         // then
         Assertions.assertEquals(expectedPage, actualOutput.currentPage());
         Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
-        Assertions.assertEquals(expectedTotal, actualOutput.totalItems());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
         Assertions.assertEquals(expectedItems, actualOutput.items());
 
         Mockito.verify(genreGateway, times(1)).findAll(eq(aQuery));
@@ -106,7 +106,7 @@ public class ListGenreUseCaseTest extends UseCaseTest {
         // then
         Assertions.assertEquals(expectedPage, actualOutput.currentPage());
         Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
-        Assertions.assertEquals(expectedTotal, actualOutput.totalItems());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
         Assertions.assertEquals(expectedItems, actualOutput.items());
 
         Mockito.verify(genreGateway, times(1)).findAll(eq(aQuery));
